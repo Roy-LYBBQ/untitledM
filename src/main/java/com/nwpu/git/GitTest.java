19e7ff1c1fe5be713ab36978215f88ac18850056
Roy <1554887871@qq.com>
2022-12-20 16:19:36 +0800

    first commit

---
+++ src/main/java/com/nwpu/git/GitTest.java
@@ -0,0 +1,14 @@
+package com.nwpu.git;
+
+/**
+ * GitTest
+ *
+ * @author 龙虹宇
+ * @version 1.0
+ * &#064;date  2022/12/20
+ */
+public class GitTest {
+    public static void main(String[] args) {
+        System.out.println("hello , git");
+    }
+}
