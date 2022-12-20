5d3b5150797e30f49a6c14cc03e77582ac2190ed
Roy-LYBBQ <115216702+Roy-LYBBQ@users.noreply.github.com>
2022-12-20 18:26:28 +0800

    pull commit

--- src/main/java/com/nwpu/git/GitTest.java
+++ src/main/java/com/nwpu/git/GitTest.java
@@ -16,5 +16,6 @@ public class GitTest {
         System.out.println("master test");
         System.out.println("hot-fix");
         System.out.println("push");
+        System.out.println("pull");
     }
 }
